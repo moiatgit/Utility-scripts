83ea3f862a4bcc03a5474994f0051bf148ceeac7
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2012-01-25 13:01:52 +0100

    corrected bug in notifica_forum.sh

--- notifica_forum.sh
+++ notifica_forum.sh
@@ -3,6 +3,9 @@
 # and if there're any unread entries it shows it as a notification.
 # It uses notify-send utility from libnotify-bin package
 #
+# Put a call to this script at the startup application list if you
+# want to get notifications when login
+#
 # TODO: replace the need of temporary file (load response on memory)
 #
 fitxer=/tmp/ies_checkforums.py.resposta
