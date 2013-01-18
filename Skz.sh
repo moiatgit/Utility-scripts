db2a9c942f3f2bd1679561db6deaeb23f1b7743c
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2013-01-18 10:22:13 +0100

    added just backup capability by prepending -- to pen name

--- Skz.sh
+++ Skz.sh
@@ -3,13 +3,18 @@
 # Synchronization script
 #
 # tries to synchronize local repository with its pen's counterpart
-# It accepts one or more pendrives
+# It adds also the possiblity to just backup (JBK) without expecting
+# changes in destination repository
+#
+# It accepts one or more pendrives and jbk
 #
 # ~/.Skz should contain configuration in the following format:
 #    LOCAL="«path to local rep"
 #    PEN=("«path to one pendrive»" "«path to another pendrive»")
 #    LOCAL_NAME=«host name rep»
 #    PEN_NAME=("«pen1 name rep»" "«pen2 name rep»")
+# When pen name repository starts with -- Skz treats as JBK and no commit is
+# performed on this repository.
 #
 # TODO: add robustness
 #       add option to prepare a repositori from given path and add it to
@@ -57,12 +62,17 @@ then
         if [ -d "$pen_path" ];
         then
             rep_name=${PEN_NAME[$i]}
-            commit $pen_path
-            cd $LOCAL
-            echo "··· Pulling from $pen_path"
-            git pull $rep_name master
+            if [[ $rep_name == "--"* ]]; # check whether it is a just backup rep
+            then
+                rep_name=${rep_name:2}  # extract -- from repository name
+            else
+                commit $pen_path
+                cd $LOCAL
+                echo "··· Pulling from $rep_name to $LOCAL_NAME"
+                git pull $rep_name master
+            fi
             cd $pen_path
-            echo "··· Pulling from $LOCAL"
+            echo "··· Pulling from $LOCAL_NAME to $rep_name"
             git pull $LOCAL_NAME master
         fi
         let "i++"
