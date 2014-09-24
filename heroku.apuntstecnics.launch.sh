4f09bc377bd8f1c3b35984e960e86d02842af393
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2014-09-24 11:43:19 +0200

    removed heroku stuff

--- heroku.apuntstecnics.launch.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Use this script to launch a http server for 'apunts tècnics'"
-echo "press a key to continue <ctrl>-c to cancel"
-read resposta
-
-cd ~/Feina/heroku.apuntstecnics
-python -m SimpleHTTPServer
