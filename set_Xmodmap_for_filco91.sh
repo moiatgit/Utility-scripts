d15980640ec7e0a36fa24a87b2e9bf2dde6dc238
Moisès Gómez <moi@blamihome.blami.org>
2015-05-14 17:02:07 +0200

    fixed keymapping and added a direct keymap option

--- set_Xmodmap_for_filco91.sh
+++ set_Xmodmap_for_filco91.sh
@@ -1,14 +1,31 @@
 #! /bin/bash
-Xmodmapfile="$HOME/.Xmodmap" 
 echo "Sets the ~/.Xmodmap file to remap Filco's keys"
-if [ -f "$Xmodmapfile" ];
+if [[ "$1" == "!" ]];
 then
-    echo "File $Xmodmapfile already exists. No changes"
-else
-    cat > "$Xmodmapfile" << END
+    echo "Es procedeix a executar els remappings directament per a aquesta sessió"
+    xmodmap -e "keycode 102 = ISO_Level3_Shift"
+    xmodmap -e "keycode 100 = ISO_Level3_Shift"
+    xmodmap -e "keycode 101 = Alt_R"
+    xmodmap -e "keycode 108 = Super_L"
+    echo "Fet!"
+    exit
+fi
+if [[ "$1" == "f" ]];
+then
+    Xmodmapfile="$HOME/.Xmodmap" 
+    echo "Es procedeix a crear els mappings al fitxer $Xmodmapfile"
+    if [ -f "$Xmodmapfile" ];
+    then
+        echo "File $Xmodmapfile already exists. No changes"
+    else
+        cat > "$Xmodmapfile" << END
 keycode 102 = ISO_Level3_Shift
 keycode 100 = ISO_Level3_Shift
-keycode 101 = Super_R NoSymbol Super_R
+keycode 101 = Alt_R
+keycode 108 = Super_L
 END
-    echo "$Xmodmapfile has been created"
+        echo "$Xmodmapfile has been created"
+    fi
+    exit
 fi
+echo "Indica amb l'argument ! per realitzar el mapping o f per crear el fitxer"
