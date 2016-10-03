5c0794c72a6827072c223c378770e541fa882814
Moisès Gómez Girón <moiatgit@gmail.com>
2016-10-03 15:38:00 +0200

    mapping changes on laptop

--- setfilco.sh
+++ setfilco.sh
@@ -4,4 +4,4 @@ echo "Key remaping for Filco 91"
 setxkbmap -layout es -model 105
 #switch_keyboard_filco.sh
 #set_Xmodmap_for_filco91.sh goca
-xmodmap $HOME/.Xmodmap
+xmodmap $HOME/bin/xmodmap.ca.filco91
