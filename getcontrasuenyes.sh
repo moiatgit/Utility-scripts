29bf17f27cfe467f12fc4d4ea6508ca73d8a7cc5
Moisès Gómez Girón <moiatgit@gmail.com>
2016-10-25 17:31:57 +0200

    removed shufflequiz link

--- getcontrasuenyes.sh
+++ getcontrasuenyes.sh
@@ -2,7 +2,12 @@
 ping -c 1 blamihost.blami.net &> /dev/null
 if [[ "$?" == "0" ]];
 then
-    scp -p moi@blamihost.blami.net:.contrasuenyes $HOME/
+    if [[ "$1" == "send" ]];
+    then
+        scp -p $HOME/.contrasuenyes moi@blamihost.blami.net:.contrasuenyes
+    else
+        scp -p moi@blamihost.blami.net:.contrasuenyes $HOME/
+    fi
 else
     echo "out"
 fi
--- shufflequiz.py
+++
@@ -1,1 +0,0 @@
-/home/moi/dev/shufflequiz/shufflequiz.py
