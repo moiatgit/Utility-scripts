62adecfedfae14531f785bb5e68df50fdd435ad7
moises <moiatgit@gmail.com>
2016-12-27 07:34:29 +0100

    added a simple utility to decompress any package from current folder

---
+++ recursive_unzip.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+# This script decompreses packages from current folder
+# Original packages are left there so, subsequent executions of this script will lead the
+# user to deal with the overwritting politics of each uncompressing utility
+#
+
+echo "This script will decompress any package from current folder"
+echo "press enter to continue <ctrl>-c to cancel"
+read resposta
+
+processa() {
+    path=$(dirname $1)
+    name=$(basename $1)
+    extension="${name##*.}"
+    src="../$name"
+    basedir="$PWD"
+    newdir="$name.files"
+
+    cd "$path"
+    if [[ "$name" == *.jar ]];
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        jar xvf "$src"
+    elif [[ "$name" == *.tar.gz ]];
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        tar xzvf "$src"
+    elif [[ "$name" == *.gz ]];
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        tar xzvf "$src"
+    elif [[ "$name" == *.zip ]];
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        jar xvf "$src"
+    elif [[ "$name" == *.rar ]];
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        unrar x "$src"
+    fi
+
+    cd "$basedir"
+}
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+#files=`find . -type f \( -name '*.zip' -o -name '*.tar.gz' -o -name '*.jar' \)`
+files=`find . -type f`
+for f in $files;
+do
+    processa "$f"
+done
+IFS=$SAVEIFS
+
