c686f3c007ab8b0c7325d71ac0670c3d434c5f22
moises <moiatgit@gmail.com>
2017-01-15 06:02:12 +0100

    javaclean.sh now requres confirmation

--- javaclean.sh
+++ javaclean.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 echo "Clean up of java classes and temporary files (*~)"
+echo "ctrl-c to abort"
+read resposta
 find . -type f -name '*.class' -delete
 find . -type f -name '*~' -delete
