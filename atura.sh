5214486271fa80a3990105aee69e7ecd6de38fc6
moises <moiatgit@gmail.com>
2017-04-12 13:04:38 +0200

    added atura.sh to allow delayed suspension

---
+++ atura.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# This script requires that '$ sudo pm-suspend' doesn't require
+# password.
+# If you can afford the risk in your system, you can simply add the
+# following line to visudo
+#
+#   «yourusername» ALL=(root) NOPASSWD: /usr/sbin/pm-suspend
+
+
+
+
+echo -n "Suspending the system "
+if [[ -z "$1" ]];
+then
+    echo "now"
+    sudo pm-suspend
+else
+    echo "in $1"
+    sleep "$1" && sudo pm-suspend
+fi
+
