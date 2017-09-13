466cdf2cd1327344f9dce89e912767d76442955b
moises <moiatgit@gmail.com>
2017-09-13 15:56:13 +0200

    added branch awareness

--- sincroniza.ies-contiguts.sh
+++ sincroniza.ies-contiguts.sh
@@ -1,24 +1,26 @@
 #! /bin/sh
 # Sincronitza ies-continguts
-# Espera a ~/.sincro.ies-continguts la definició de $repositories
+# Espera a ~/.sincro.ies-continguts la definició de $repositories i $branches
 
-# TODO: es pot generalitzar a la resta de repositoris si fas que les branques siguin obtingudes dels
+# TODO: es pot generalitzar a la resta de repositoris ara que les branques són obtingudes dels
 # diferents repositoris git directament.
 
 sincroniza(){
     echo "Sincronització de continguts amb $1"
+    startbranch=`env LANG=en git status | grep "On branch" | cut -d " " -f 3`
     if [ ! -d "$1" ];
     then
         echo "No està accessible $1"
     else
         cd "$1"
-        for branch in master online oldstuff dev-cursxml;
+        for branch in $branches;
         do
+            echo "Syncing on $branch"
             git checkout $branch
             bash gitpull.sh
             bash gitpush.sh
         done
-        git checkout master
+        git checkout $startbranch
     fi
 }
 
