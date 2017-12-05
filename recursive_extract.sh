3574b256b2170e61b017802e12cf0e9f5c56ac77
Moisès Gómez Girón <moiatgit@gmail.com>
2017-12-05 18:59:28 +0100

    added recursive_extract.sh as a replacement of recursive_unzip.sh that allows uncompressing files using their mime type instead of extension

--- recursive_extract.sh
+++ recursive_extract.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 # This script decompreses packages from current folder
-# The uncompressing algorithm is decided after the file extension
+# It considers the mime-type of the file to decide the alrogithm
+#
 # Original packages are left there so, subsequent executions of this script will lead the
 # user to deal with the overwritting politics of each uncompressing utility
 #
+
 echo "This script will decompress any package from current folder"
 echo "press enter to continue <ctrl>-c to cancel"
 read resposta
@@ -13,47 +15,50 @@ processa() {
     path=$(dirname $1)
     name=$(basename $1)
 
+    mimetype=`file --mime-type "$1" | rev | cut -d ':' -f 1 | rev | tr -d [:space:]`
+    echo "XXX $mimetype"
+
     extension="${name##*.}"
     src="../$name"
     basedir="$PWD"
     newdir="$name.files"
 
     cd "$path"
-    if [[ "$name" == *.jar ]];
+    if [[ "$mimetype" == "application/java-archive" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         jar xvf "$src"
-    elif [[ "$name" == *.tar.bz2 ]];
+    elif [[ "$mimetype" == "application/x-bzip2" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         tar xjvf "$src"
-    elif [[ "$name" == *.tar.gz ]];
+    elif [[ "$mimetype" == "application/gzip" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         tar xzvf "$src"
-    elif [[ "$name" == *.gz ]];
-    then
-        mkdir -p "$newdir"
-        cd "$newdir"
-        tar xzvf "$src"
-    elif [[ "$name" == *.tar ]];
+    elif [[ "$mimetype" == "application/x-tar" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         tar xvf "$src"
-    elif [[ "$name" == *.zip ]];
+    elif [[ "$mimetype" == "application/zip" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         jar xvf "$src"
-    elif [[ "$name" == *.rar ]];
+    elif [[ "$mimetype" == "application/x-rar" ]]
     then
         mkdir -p "$newdir"
         cd "$newdir"
         unrar x "$src"
+    elif [[ "$mimetype" == "application/x-7z-compressed" ]]
+    then
+        mkdir -p "$newdir"
+        cd "$newdir"
+        7za e "$src"
     fi
 
     cd "$basedir"
