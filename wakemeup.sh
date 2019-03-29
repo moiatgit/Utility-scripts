aadb230d158744469a456c2b3fad8e38c53d49b7
moises <moiatgit@gmail.com>
2019-03-29 20:19:20 +0100

    fixed a problem with the path to the sound file

--- wakemeup.sh
+++ wakemeup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+basedir=$(dirname $0)
+soundfilename=bell.wav
 defaulttime=10m
 timetowait=${1:-$defaulttime}
 echo "This script will play a sound after $timetowait"
-$(sleep $timetowait; paplay bell.wav) &
+$(sleep $timetowait; paplay "$basedir/$soundfilename") &
