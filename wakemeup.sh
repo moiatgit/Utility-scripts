49cf623143368f0c8cb13c56f75418d0c99d35b1
moises <moiatgit@gmail.com>
2019-04-10 10:28:18 +0200

    added sound at the beginning of the execution to allow checking if available

--- wakemeup.sh
+++ wakemeup.sh
@@ -3,5 +3,6 @@ basedir=$(dirname $0)
 soundfilename=bell.wav
 defaulttime=10m
 timetowait=${1:-$defaulttime}
+paplay "$basedir/$soundfilename"
 echo "This script will play a sound after $timetowait"
 $(sleep $timetowait; paplay "$basedir/$soundfilename") &
