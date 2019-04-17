1d82b946e99fd5a9d4833d1d4af336acec1cc171
moises <moiatgit@gmail.com>
2019-04-17 16:22:14 +0200

    updated neteja for current debian cache folder distribution

--- neteja.sh
+++ neteja.sh
@@ -2,4 +2,6 @@ echo "Cleaning temporary files"
 find ~/.thumbnails/  -type f -exec rm -v '{}' \;
 rm -rvf ~/.local/share/Trash/*
 #python ~/bin/netejarecent.py -a
+find ~/.cache/shotwell -type f -exec rm -v {} \;
+find ~/.cache/thumbnails  -type f -exec rm -v '{}' \;
 echo "Cleaning done!"
