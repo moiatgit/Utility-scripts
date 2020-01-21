a38a5aa975cf82ba0488b28bc62d64a59335fb38
Moisès Gómez Girón <moiatgit@gmail.com>
2020-01-21 18:20:58 +0100

    simplified iptables rule

--- restrict_user.sh
+++ restrict_user.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
-echo "This script restricts user access to different resources"
+if [ -z "$1" ];
+then
+    owner=test
+else
+    owner=$1
+fi
+echo "This script restricts user $owner access to different resources"
 echo "It requires sudo privileges"
-sudo iptables -I OUTPUT -p tcp -m owner --uid-owner test -j DROP
-sudo iptables -I OUTPUT -p udp -m owner --uid-owner test -j DROP
-sudo iptables -I OUTPUT -p icmp -m owner --uid-owner test -j DROP
+sudo iptables -I OUTPUT -m owner --uid-owner $owner -j DROP
+#sudo iptables -I OUTPUT -p tcp -m owner --uid-owner $owner -j DROP
+#sudo iptables -I OUTPUT -p udp -m owner --uid-owner $owner -j DROP
+#sudo iptables -I OUTPUT -p icmp -m owner --uid-owner $owner -j DROP
