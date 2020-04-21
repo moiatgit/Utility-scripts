20eda9e895a42d9a844ae91b1be60f7b4d36d9b5
Moisès <moisesgomezgiron@gmail.com>
2020-04-21 10:17:44 +0200

    added script to update nvim

--- convert_video2mp4.sh
+++ convert_video2mp4.sh
@@ -16,3 +16,18 @@ do
         fi
     fi
 done
+
+
+# Here a script that converted the files from a temporary folder
+# to a main profile
+#for f in $*;
+#do
+#    for i in $f/*.mp4;
+#    do
+#        echo $i
+#        nom=`basename "$i"`
+#        nice HandBrakeCLI -i "$i" -o "$nom" --encoder-profile main
+#        nice mv -v "$nom" "$i"
+#    done
+#done
+#
---
+++ update_nvim.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+cd $HOME/soft/nvim.versions
+if [[ "$1" == "nightly" ]];
+then
+    version=nightly
+else
+    version=stable
+fi
+
+echo "neovim upgrader to $version"
+
+mkdir -p $version
+cd $version
+curl -LO https://github.com/neovim/neovim/releases/download/$version/nvim.appimage
+chmod u+x nvim.appimage
+
+ln -f -s -r $(realpath nvim.appimage) $HOME/soft/nvim
