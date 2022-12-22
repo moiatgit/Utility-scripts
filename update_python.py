85bcbc69de7c75ad98a380adee8c977fbc329ec2
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2022-12-22 18:43:35 +0100

    more automation on update_python.py script

--- update_python.py
+++ update_python.py
@@ -1,12 +1,40 @@
 #! /bin/bash
 echo "This script updates python"
+if [ -z "$1" ];
+then
+    echo "Use $0 download-url"
+    echo "\t e.g. $0 https://www.python.org/downloads/release/python-3111/"
+exit 1
+fi
+zipped=$(basename $1)
+foldername=${zipped%.*}
+echo $foldername
+cd /tmp
+if ! wget -c "$1";
+then
+    echo "Error downloading $1"
+    exit 1
+fi
+if [! -f $zipped ];
+then
+    echo "Error: downloaded file not found $zipped"
+    exit 1
+fi
+if [ ! -d $foldername ];
+then 
+    tar xzvf $zipped
+fi
+if [ ! -d $foldername ];
+then
+    echo "Error: expected folder name $foldername"
+    exit 1
+fi
+cd $foldername
 if [ ! -f configure ];
 then
-    echo "First of all, download and untar the new version"
-    echo "Then cd into the new folder and rerun this script"
-else
-    ./configure --enable-optimizations && \
-        make -j `nproc` && \
-        sudo make altinstall
+    echo "Error: expected file $foldername/configure"
+    exit 1
 fi
-
+./configure --enable-optimizations && \
+    make -j `nproc` && \
+    sudo make altinstall
