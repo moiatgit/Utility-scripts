9da2fadbeb2cac4aa40907b773ff3af2cc6bcaf4
Moises <moisesgomezgiron@gmail.com>
2023-12-28 22:15:24 +0100

    removed requirement of gvim

--- contrasuenyes.sh
+++ contrasuenyes.sh
@@ -1,2 +1,14 @@
 #! /bin/sh
-gvim $HOME/.contrasuenyes
+vimflavor=vim
+filetoopen=$HOME/.contrasuenyes
+
+TERMINAL=gnome-terminal     # xfce4-terminal
+
+version5=$(LANG=en bash --version | grep "GNU bash, version 5")
+if [ -z "$version5" ];
+then
+    $TERMINAL -e "$vimflavor -p \"$filetoopen\""
+else
+    OPTION="-q"
+    $TERMINAL $OPTION -- $vimflavor -p "$filetoopen"
+fi
