a9dceb1b252b2f8cd32a28e32ac13bdd38c638cd
Moises <moisesgomezgiron@gmail.com>
2023-12-29 13:02:24 +0100

    now it is able to remove some noisy keybindings that where preventing direct access to workspaces

--- set_gnome3_preferences.sh
+++ set_gnome3_preferences.sh
@@ -4,16 +4,16 @@
 # Sets the shortcuts to access the different workspaces
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
 #
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1  "['<Ctrl><Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2  "['<Ctrl><Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3  "['<Ctrl><Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4  "['<Ctrl><Super>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5  "['<Ctrl><Super>5']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6  "['<Ctrl><Super>6']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7  "['<Ctrl><Super>7']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8  "['<Ctrl><Super>8']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9  "['<Ctrl><Super>9']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Ctrl><Super>0']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1  "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2  "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3  "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4  "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5  "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6  "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7  "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8  "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9  "['<Super>9']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
 
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1  "['<Super><Shift>1']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2  "['<Super><Shift>2']"
@@ -38,3 +38,19 @@ gsettings set org.gnome.desktop.interface enable-hot-corners false
 
 # menubar detachable
 gsettings set org.gnome.desktop.interface menubar-detachable false
+
+# remove noisy shell keybindings
+gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>v']"
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
+
+# maximize
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>m']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super><Shift>m']"
