6e5fa5892c38d7a685436ada4dc8441490490bb6
Moises <moises@x270.blami.net>
2024-02-25 09:03:21 +0100

    added a getter for contrasuenyes

---
+++ get_contrasuenyes.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p ~/.config/contrasuenyes
+cd ~/.config/contrasuenyes
+if [ -f .contrasuenyes ];
+then
+    mv .contrasuenyes ".contrasuenyes.$(date +%Y%m%d%H%M%S)"
+fi
+scp blami.net:.config/contrasuenyes/.contrasuenyes .
